a61713131b3eb7cdd7a1d948cb1f4758c9473831
Tory <88531407+1two13@users.noreply.github.com>
2022-03-27 19:28:40 +0900

    features: 코드 작성 완료

--- airport.js
+++ airport.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
+// 동기적으로 파일 읽기
 const airportString = fs.readFileSync('airport.json', 'utf-8');
+// 파일을 문자열 => JSON 으로 변경
 const airport = JSON.parse(airportString);
-
+// airport 파일 exports
 module.exports = airport;
--- index.js
+++ index.js
@@ -8,14 +8,50 @@ app.get('/', (req, res) => {
 })
 
 app.get('/airport', (req, res) => {
-	res.send(airport);
+	res.send(Object.keys(airport).map(code=>airport[code]));
 })
 
-app.get('/search', (req, res) => {
+app.get('/search/name', (req, res) => {
 	const name = req.query.name;
-	res.send(req.query)
+	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
+	const arr = countryList.filter(element => 
+		element.name.includes(name) || 
+		element.korean.includes(name) ||
+		element.english.toUpperCase().includes(name.toUpperCase())
+	);
+	res.send(arr)
 })
 
+app.get('/search/code', (req, res) => {
+	const code = req.query.code;
+	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
+	const arr = countryList.filter(element => element.IATA.includes(code.toUpperCase()));
+	
+	// let i = 0;
+	// let arr = [];
+	// while(i < countryList.length) {
+	// 	if(countryList[i].IATA.includes(code)) {
+	// 		arr.push(countryList[i]);
+	// 	}
+	// 	i++
+	// }
+	res.send(arr)
+})
+
+app.get('/search/country', (req, res) => {
+	const country = req.query.country; // query가 country인 값
+	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
+	
+	let i = 0;
+	let arr = [];
+	while(i < countryList.length) {
+		if(countryList[i].country.includes(country)) {
+			arr.push(countryList[i]);
+		}
+		i++
+	}
+	res.send(arr);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
