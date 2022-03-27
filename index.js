c501c0c5c64518f70183fdb55cc8d1816ae2636a
Tory <88531407+1two13@users.noreply.github.com>
2022-03-27 22:20:14 +0900

    features: 코드 주석 작성 완료

--- index.js
+++ index.js
@@ -8,51 +8,44 @@ app.get('/', (req, res) => {
 })
 
 app.get('/airport', (req, res) => {
+	// console.log(airport)
 	res.send(Object.keys(airport).map(code=>airport[code]));
 })
 
 app.get('/search/name', (req, res) => {
 	const name = req.query.name;
-	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
+	const countryList = Object.values(airport) 
 	const arr = countryList.filter(element => 
 		element.name.includes(name) || 
 		element.korean.includes(name) ||
 		element.english.toUpperCase().includes(name.toUpperCase())
 	);
-	res.send(arr)
+	res.send(arr);
 })
 
 app.get('/search/code', (req, res) => {
 	const code = req.query.code;
-	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
-	const arr = countryList.filter(element => element.IATA.includes(code.toUpperCase()));
+	const countryList = Object.values(airport) 
+	const arr = countryList.filter(el => el.IATA.includes(code.toUpperCase()));
 	
-	// let i = 0;
-	// let arr = [];
-	// while(i < countryList.length) {
-	// 	if(countryList[i].IATA.includes(code)) {
-	// 		arr.push(countryList[i]);
-	// 	}
-	// 	i++
-	// }
-	res.send(arr)
+	res.send(arr);
 })
 
 app.get('/search/country', (req, res) => {
-	const country = req.query.country; // query가 country인 값
-	const countryList = Object.values(airport) // 객체를 배열로 바꾸는 코드
+	const country = req.query.country; // query(? 뒤에 오는 문자)가 country인 것의 값 
+	const countryList = Object.values(airport); // 객체를 배열로 바꾸는 코드
 	
 	let i = 0;
 	let arr = [];
 	while(i < countryList.length) {
 		if(countryList[i].country.includes(country)) {
 			arr.push(countryList[i]);
-		}
+		};
 		i++
 	}
 	res.send(arr);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
