4f302328c1f6f167a01f8df6c6706ae60b2a4bc5
Tory <88531407+1two13@users.noreply.github.com>
2022-04-05 14:13:07 +0900

    features: tab 수정

--- airport.js
+++ airport.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // 동기적으로 파일 읽기
-const airportString = fs.readFileSync('airport.json', 'utf-8');
+const airportString = fs.readFileSync("airport.json", "utf-8");
 // 파일을 문자열 => JSON 으로 변경
 const airport = JSON.parse(airportString);
 // airport 파일 exports
--- index.js
+++ index.js
@@ -1,75 +1,33 @@
-const express = require('express');
-const airport = require('./airport');
-const cors = require('cors'); 
+const express = require("express");
+const airport = require("./airport");
+const cors = require("cors");
 const app = express();
 const port = 8080;
 
 app.use(cors()); // 모든 요청에 대해 cors 허용시키기
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/airport', (req, res) => {
-	// console.log(airport)
-	res.send(Object.keys(airport).map(code=>airport[code]));
-})
-
-// 공항코드
-app.get('/search/code', (req, res) => {
-	const code = req.query.code;
-	const countryList = Object.values(airport) 
-	const arr = countryList.filter(el => el.IATA.includes(code.toUpperCase()));
-	
-	res.send(arr);
-})
-
-// 나라
-app.get('/search/country', (req, res) => {
-	const country = req.query.country; // query(? 뒤에 오는 문자)가 country인 것의 값 
-	const countryList = Object.values(airport); // 객체를 배열로 바꾸는 코드
-	
-	let i = 0;
-	let arr = [];
-	while(i < countryList.length) {
-		if(countryList[i].country.includes(country)) {
-			arr.push(countryList[i]);
-		};
-		i++
-	}
-
-	res.send(arr);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// 위치
-app.get('/search/location', (req, res) => {
-	const location = req.query.location; // query(? 뒤에 오는 문자)가 country인 것의 값 
-	const locationList = Object.values(airport); // 객체를 배열로 바꾸는 코드
-
-	let i = 0;
-	let arr = [];
-	while(i < locationList.length) {
-		if(locationList[i].location.includes(location)) { 
-			arr.push(locationList[i]);
-		};
-		i++
-	}
-
-	res.send(arr);
+app.get("/airport", (req, res) => {
+  res.send(Object.keys(airport).map((code) => airport[code]));
 });
 
-// 공항이름 (korean && english)
-app.get('/search/name', (req, res) => {
-	const name = req.query.name;
-	const countryList = Object.values(airport) 
-	const arr = countryList.filter(element =>  
-		element.korean.includes(name) ||
-		element.english.toUpperCase().includes(name.toUpperCase())
-	);
+app.get("/search", (req, res) => {
+  const query = req.query.query;
+  const list = Object.values(airport);
+  const arr = list.filter(
+    (el) =>
+      el.IATA.includes(query?.toUpperCase()) ||
+      el.country.includes(query) ||
+      el.location.includes(query) ||
+      el.korean.includes(query) ||
+      el.english.toUpperCase().includes(query?.toUpperCase())
+  );
 
-	res.send(arr);
-})
-
+  res.send(arr);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
